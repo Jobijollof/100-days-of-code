262e820bef80fc1838d3f219a27c06c757620894
Jobijollof <jobinaibegbulem@yahoo.com>
2023-01-09 15:39:43 +0100

    python-100days of code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # 100-days-of-code
 100 days of Code: The complete python bootcamp for 2023.
+
+I shall be documenting all my learnings projects and observations as i code for the next 100 days.
+
+
+
---
+++ day1.py
@@ -0,0 +1,21 @@
+# Exercise 1: Printing
+#print("Day 1 - Python print function")
+#print("The function is declared like this:")
+#print("print('what to print')")
+
+# use back slash \n to use a single print statement to print multiple lines.
+
+#print("Hello world!\nHello world!\nHello world!")
+
+# Concatenate strings using the plus sign. To add a space, leave a space after hello or before Jobina or " "
+#print("hello" + "Jobina")
+#print ("Hello " + "Jobina")
+#print ("Hello" + " Jobina")
+#print ("Hello"+ " "+ "Jobina")
+
+#Indentation is very important in python.(indentation error)
+# Syntax is important(Syntax error)
+
+#Exercise 2: Input function( A prompt for the user)
+#input("what is your name?")
+print("Hello" + " " + input("What is your name? " ))
