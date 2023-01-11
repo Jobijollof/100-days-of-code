6575408e0edb01c0d7751861df7c7fff4bebea89
Jobijollof <jobinaibegbulem@yahoo.com>
2023-01-11 17:09:02 +0100

    100- DAYS- OF- CODE

--- day1.py
+++ day1.py
@@ -6,7 +6,7 @@ print("print('what to print')")
 
 # use back slash and n (\n) to use a single print statement to print multiple lines.
 
-#print("Hello world!\nHello world!\nHello world!")
+print("Hello world!\nHello world!\nHello world!")
 
 # Concatenate strings using the plus sign. To add a space, leave a space after hello or before Jobina or " "
 
@@ -16,7 +16,7 @@ print ("Hello" + " Jobina")
 print ("Hello"+ " "+ "Jobina")
 
 #Indentation is very important in python.(indentation error)
-# Syntax is important(Syntax error)
+#Syntax is important(Syntax error)
 
 #Exercise 2: Input function. input() will get user to input in console
 #Then print () will print hello and the user input
---
+++ day2.py
@@ -0,0 +1,11 @@
+# Data Types, Numbers, Operators, Type conversion,f-strings
+
+# Data Types
+
+# Strings
+
+#print("Hello")
+
+"Hello"[0]
+
+
