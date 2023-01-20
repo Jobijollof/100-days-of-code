f003c17089841251ca5cd09d882337903e6293ed
Jobijollof <jobinaibegbulem@yahoo.com>
2023-01-20 11:42:11 +0100

    100-days-of- code

--- day3.py
+++ day3.py
@@ -34,22 +34,36 @@
  
 #If/Else Elif Statement.
 
-print("Welcome to the Rollercoaster!")
-height= int(input("What is your height in cm? "))
-if height >= 120:
-    print("You get a ticket")
-    age = int(input("input your age "))
-    if age >= 18:
-        print("You are paying the full adult price")
-    elif age <18 == 12:
-        print("You are paying 7$")
-    elif age <= 12:
-        print("You are paying 2$")
-else:
-    print("You need to grow taller")
+# print("Welcome to the Rollercoaster!")
+# height= int(input("What is your height in cm? "))
+# if height >= 120:
+#     print("You get a ticket")
+#     age = int(input("input your age "))
+#     if age >= 18:
+#         print("You are paying the full adult price")
+#     elif age <18 == 12:
+#         print("You are paying 7$")
+#     elif age <= 12:
+#         print("You are paying 2$")
+# else:
+#     print("You need to grow taller")
     
+    
+    
+#BMI 2.0 Exercise:
+height = float(input("Enter your height in m: "))
+weight = float (input("Enter your weight in kg: "))
+bmi = round(weight / height **2)
+if bmi < 18.5:
+    print(f" Your bmi is {bmi}, You are underweight")
+elif bmi < 25:
+    print(f" Your bmi is {bmi}, You have a  normal weight.")
+elif bmi < 30:
+    print(f"Your bmi is {bmi}, You are over weight")
+elif bmi < 35:
+    print(f"Your bmi is {bmi}, You are Obese")
+else:
+    print(f"Your bmi is {bmi}, you are clinically obese") 
 
 
 
-#else: 
-    #print("You need to grow taller")
