fb6ee1862924e5f4380cd09fe9327756cb80960b
Jobijollof <jobinaibegbulem@yahoo.com>
2023-01-21 13:38:19 +0100

    100-days-of- code

--- day3.py
+++ day3.py
@@ -66,14 +66,35 @@
 #     print(f"Your bmi is {bmi}, you are clinically obese") 
 
 #Leap Year exercise:
-year= int(input("What year do you wish to check? "))
-if year % 4 == 0:
-    print("Leap year")
-elif year % 100 == 0:
-    print("Leap year")
-elif year % 400 == 0:
-    print("Leap year")
+# year= int(input("What year do you wish to check? "))
+# if year % 4 == 0:
+#     print("Leap year")
+# elif year % 100 == 0:
+#     print("Leap year")
+# elif year % 400 == 0:
+#     print("Leap year")
+# else:
+#     print("Not Leap year")
+
+
+print("Welcome to the Rollercoaster!")
+height= int(input("What is your height in cm? "))
+bill = 0
+if height >= 120:
+     print("You get a ticket")
+     age = int(input("input your age "))
+     if age >= 18:
+         bill = 12
+         print("Adult ticket $12")
+     elif age <18 == 12:
+         bill = 7
+         print("Youth ticket $7")
+     elif age <= 12:
+         bill = 2
+         print("Child ticket $2")
+     wants_photo = input("Do you want a photo taken? Y or N.")
+     if wants_photo == "Y":
+        bill = bill + 3
+        print(f"Your final bill is {bill}")
 else:
-    print("Not Leap year")
-
-
+    print("You need to grow taller")
