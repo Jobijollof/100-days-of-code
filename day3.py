f0528fdcd7012b81c17a26c8d2812c575d05b3ec
Jobijollof <jobinaibegbulem@yahoo.com>
2023-01-23 11:56:05 +0100

    100-days-of- code

--- day3.py
+++ day3.py
@@ -99,61 +99,85 @@
 # else:
 #     print("You need to grow taller")
     
-# Pizza Order Exercise:
-print("welcome to python pizza deliveries") 
-size= input(" What size of pizza do you want S, M, or L ")
-add_peperoni=input("Do you want peperoni? Y or N ")
-extra_cheese= input("Do you want extra cheese? Y or N ")
-bill = 0
-if size == "S":
-    bill = 15
-    print(f"Your bill is ${bill} ")
-elif size == "M":
-    bill = 20
-    print(f"Your bill is ${bill}")
-elif size == "L":
-    bill=25
-    print(f"Your bill is ${bill}")
-if add_peperoni == "Y":
-    if size == "S":
-     bill = bill + 2
-    else:
-     bill = bill + 3
-    print(f"Your final bill is ${bill}")
-elif extra_cheese == "Y": 
-    bill = bill + 3
-    print(f"Your final bill is {bill}")
-else:
-    print("Thank you for stopping by")
+# # Pizza Order Exercise:
+# print("welcome to python pizza deliveries") 
+# size= input(" What size of pizza do you want S, M, or L ")
+# add_peperoni=input("Do you want peperoni? Y or N ")
+# extra_cheese= input("Do you want extra cheese? Y or N ")
+# bill = 0
+# if size == "S":
+#     bill = 15
+#     print(f"Your bill is ${bill} ")
+# elif size == "M":
+#     bill = 20
+#     print(f"Your bill is ${bill}")
+# elif size == "L":
+#     bill=25
+#     print(f"Your bill is ${bill}")
+# if add_peperoni == "Y":
+#     if size == "S":
+#      bill = bill + 2
+#     else:
+#      bill = bill + 3
+#     print(f"Your final bill is ${bill}")
+# elif extra_cheese == "Y": 
+#     bill = bill + 3
+#     print(f"Your final bill is {bill}")
+# else:
+#     print("Thank you for stopping by")
 
 # Logical operators (Combining  multiple conditions):
 #And (both conditions have to be true or else it amounts to false)
 # Or (if just one condition is true or are both true. This evaluates to false when both statements are false)
 # Not(reverses a condition )
 
-print("Welcome to the Rollercoaster!")
-height= int(input("What is your height in cm? "))
-bill = 0
-if height >= 120:
-    print("You get a ticket")
-    age = int(input("input your age "))
-    if age < 12:
-        bill = 5
-        print("child tickets are $5")
-        if age <= 18:
-            print("Youth tickets are $7.")
-    elif age >=45 and age <=55:
-        print("Everything is going to be ok have a free ride on us!")
-    else:
-        bill = 12
-        print("Adult tickets are $12")
-        wants_photo = input("Do you want a photo taken? Y or N.")
-        if wants_photo == "Y":
-            bill = bill + 3
-            print(f"Your final bill is ${bill}")
+#print("Welcome to the Rollercoaster!")
+# height= int(input("What is your height in cm? "))
+# bill = 0
+# if height >= 120:
+#     print("You get a ticket")
+#     age = int(input("input your age "))
+#     if age < 12:
+#         bill = 5
+#         print("child tickets are $5")
+#         if age <= 18:
+#             print("Youth tickets are $7.")
+#     elif age >=45 and age <=55:
+#         print("Everything is going to be ok have a free ride on us!")
+#     else:
+#         bill = 12
+#         print("Adult tickets are $12")
+#         wants_photo = input("Do you want a photo taken? Y or N.")
+#         if wants_photo == "Y":
+#             bill = bill + 3
+#             print(f"Your final bill is ${bill}")
 
 #Love Calculator exercise:
 print("Welcome to the love calculator")
 name1 = input("What is your name \n")
 name2 = input("What is your name? \n")
+combined_string = name1 + name2
+lower_case_string = combined_string .lower()
+t=lower_case_string.count("t")
+r=lower_case_string.count("r")
+u=lower_case_string.count("u")
+e=lower_case_string.count("e")
+true = t+r+u+e
+l=lower_case_string.count("l")
+o=lower_case_string.count("o")
+v=lower_case_string.count("v")
+e=lower_case_string.count("e")
+love = l+o+v+e
+love_score = int(str(true) +str(love))
+print(love_score)
+if (love_score < 10) or (love_score > 90):
+    print(f"Your love score is {love_score}, you go together")
+elif(love_score>=40) and (love_score<=50):
+    print(f"Your score is {love_score} happy life.") 
+else:
+    print(f"Your score is {love_score}")   
+
+
+
+
    
